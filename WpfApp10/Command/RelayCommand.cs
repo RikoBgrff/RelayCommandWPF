fed6f9fc05ef61015aca11d8d4e854fd5a7f61a3
Riko Bgrff <Riko.bagirli777@gmail.com>
2021-04-22 17:52:09 +0400

    Add project files.

---
+++ WpfApp10/Command/RelayCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp10.Command
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+
+        }
+
+        private Action<object> _execute;
+        private Predicate<object> __canExecute;
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            if(execute == null)
+            {
+                throw new NullReferenceException();
+            }
+            _execute = execute;
+            __canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return __canExecute == null ? true : __canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute.Invoke(parameter);
+        }
+    }
+        
+    }
+
---
+++ WpfApp10/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp10.Command;
+
+namespace WpfApp10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public RelayCommand MessageCommand { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            MessageCommand = new RelayCommand(Display);
+        }
+        public void Display(object text=null)
+        {
+            MessageBox.Show("Hello Guys,We have gift for you");
+        }
+    }
+}
